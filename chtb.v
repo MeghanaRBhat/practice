eff1ccd09c93000f05d4531c8bd1882d2006d77b
MeghanaRBhat <115388361+MeghanaRBhat@users.noreply.github.com>
2023-04-06 10:16:24 +0530

    Add files via upload

---
+++ chtb.v
@@ -0,0 +1,18 @@
+#Slide Switches
+NET "UpOrDown" LOC = p22; #LSB
+
+#led
+NET "Count[0]" LOC = p33; #LSB
+NET "Count[1]" LOC = p32;
+NET "Count[2]" LOC = p30;
+NET "Count[3]" LOC = p29;
+NET "Count[4]" LOC = p27;
+NET "Count[5]" LOC = p26;
+NET "Count[06]" LOC = p24;
+
+#Push Buttons
+NET "PB[4]" LOC = P45;
+NET "PB[3]" LOC = P44;
+NET "PB[2]" LOC = P43;
+NET "PB[1]" LOC = P41;
+NET "PB[0]" LOC = P40;
